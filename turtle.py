7cf748ca1810c146fdc58eb59d67622650df9da5
RyuHyunJung <83815677+RyuHyunJung@users.noreply.github.com>
2022-10-09 21:13:51 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,12 @@
+import turtle
+t = turtle.Turtle
+t.shape("turtle")
+t.forword(100)
+t.left(90)        
+t.forword(100)
+t.right(90)
+t.forword(100)
+t.right(90)
+t.forword(100)
+t.left(90)
+t.forword(100)
